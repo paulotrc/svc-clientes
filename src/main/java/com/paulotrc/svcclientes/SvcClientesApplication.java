67733fa081fc1642e58d0fc324f733099d5917b3
paulotrc <paulotrc@gmail.com>
2023-01-30 12:22:59 -0300

    [NEW] Primeiro commit da estrutura do microserviço de clientes.

---
+++ src/main/java/com/paulotrc/svcclientes/SvcClientesApplication.java
@@ -0,0 +1,13 @@
+package com.paulotrc.svcclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvcClientesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paulotrc/svcclientes/SvcClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulotrc.svcclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
