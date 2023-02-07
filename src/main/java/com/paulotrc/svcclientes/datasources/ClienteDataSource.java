012c6e21b627829d9ce67368d4f9aa0c734cdce1
paulotrc <paulotrc@gmail.com>
2023-02-07 19:21:14 -0300

    [UPDATE] Update to clean trash.

--- src/main/java/com/paulotrc/svcclientes/datasources/ClienteDataSource.java
+++ src/main/java/com/paulotrc/svcclientes/datasources/ClienteDataSource.java
@@ -5,9 +5,6 @@ import com.paulotrc.svcclientes.repositories.ClienteRepository;
 import com.paulotrc.svcclientes.repositories.MongoClienteRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
