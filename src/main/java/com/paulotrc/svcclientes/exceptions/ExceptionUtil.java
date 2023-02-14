a3bf97963211b9d29389db9eded15e4838ba5be8
paulotrc <paulotrc@gmail.com>
2023-02-13 21:48:57 -0300

    [UPDATE] update to exceptions.

--- src/main/java/com/paulotrc/svcclientes/exceptions/ExceptionUtil.java
+++ src/main/java/com/paulotrc/svcclientes/exceptions/ExceptionUtil.java
@@ -28,24 +28,23 @@ public final class ExceptionUtil {
     public static void throwException(ResourceException e) {
         switch (e.getCode().substring(0, 3)) {
             case "400":
-                throw new BadRequestException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new BadRequestException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             case "401":
-                throw new UnauthorizedException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new UnauthorizedException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             case "404":
-                throw new NotFoundException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new NotFoundException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             case "405":
-                throw new MethodNotAllowedException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new MethodNotAllowedException(e.getCode(),e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             case "406":
-                throw new NotAcceptableException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new NotAcceptableException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             case "412":
-                throw new PreconditionFailedException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new PreconditionFailedException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             case "415":
-                throw new UnsupportedMediaTypeException(e.getMessage(), e.getDeveloperMessage(), e.getCode(),
-                        e.getOrigin());
+                throw new UnsupportedMediaTypeException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             case "422":
-                throw new UnprocessableEntityException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new UnprocessableEntityException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
             default:
-                throw new InternalServerErrorException(e.getMessage(), e.getDeveloperMessage(), e.getCode(), e.getOrigin());
+                throw new InternalServerErrorException(e.getCode(), e.getMessage(), e.getDeveloperMessage(), e.getOrigin());
         }
     }
 }
